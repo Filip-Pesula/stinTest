cbe2f19c700f283a86c290e86faf5e46537f38f5
Filip-Pesula <filip.pesula@gmail.com>
2022-04-22 15:15:02 +0200

    fixed genDocs

--- CTestTestfile.cmake
+++ CTestTestfile.cmake
@@ -1,9 +1,1 @@
 include(./build/.dump/CTestTestfile.cmake)
-
-file(GLOB GCOVFILE build/.dump/CMakeFiles/*.dir/src/*.gcno)
-
-
-foreach(gcovfile ${GCOVFILE})
-    message("gcovefolder=${gcovfile}")
-    exec_program(gcovr ARGS ${gcovfile} -r . -f src --html-details Docs/Coverage/Html/coverage.html --sonarqube Docs/Coverage/Sonarqube/coverage.yml -o gcovfile/gcov)
-endforeach()
