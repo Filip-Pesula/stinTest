53f6404b6f568ffd6c56fe4e4546e281a5dfcd99
Filip-Pesula <filip.pesula@gmail.com>
2022-05-06 16:58:03 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# stinTest
+
+# Coverage
+
+https://htmlpreview.github.io/?https://github.com/Filip-Pesula/stinTest/makser/Docs/Coverage/Html/coverage.html
---
+++ src/HttpCore.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio.hpp>
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <memory>
+#include <string>
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace http = beast::http;           // from <boost/beast/http.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+
+namespace STIN_Bot {
+	void http_server(tcp::acceptor& acceptor, tcp::socket& socket);
+	
+
+	class Bot
+	{
+	public:
+		Bot();
+		~Bot();
+
+	private:
+
+	};
+
+	
+}
